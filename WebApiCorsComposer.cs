62e12fc448ac188c7aabf737e9a439886ba09760
Nasim Roshan <nasimr.dev@gmail.com>
2021-02-04 13:59:41 +0100

    Add latest changes

---
+++ WebApiCorsComposer.cs
@@ -0,0 +1,31 @@
+﻿
+
+using System.Web.Http;
+using Umbraco.Core;
+using Umbraco.Core.Composing;
+
+namespace MyTwitty
+{
+    [RuntimeLevel(MinLevel = RuntimeLevel.Run)]
+
+    public class WebApiCorsComposer : IComposer
+    {
+
+        public void Compose(Composition composition)
+        {
+            composition.Components().Insert<WebApiCorsComponent>();
+        }
+
+        public class WebApiCorsComponent : IComponent
+        {
+            public void Initialize()
+            {
+                GlobalConfiguration.Configuration.EnableCors();
+            }
+
+            public void Terminate()
+            {
+            }
+        }
+    }
+}
